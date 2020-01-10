63879dee57510d5f919ba02fa0706cda7fb6fa6c
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-01-10 15:34:21 +0000

    Update build script to support Emscripten

--- build.js
+++ build.js
@@ -1,13 +1,29 @@
 var $ = require('shelljs')
 
-if (!$.which('docker')) {
-  $.echo(
-    'This template requires docker to work. Please install docker and try again.',
-  )
-  $.exit(1)
+// Emscripten command:
+var cmd = `
+  emcc -O2 -s WASM=1 \
+    ./src/main.c \
+    -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap", "setValue"]' \
+    -s ALLOW_MEMORY_GROWTH=1 \
+    -s DYNAMIC_EXECUTION=0 \
+    -s TEXTDECODER=0 \
+    -s MODULARIZE=1 \
+    -s ENVIRONMENT="web" \
+    -s EXPORT_NAME="emscripten" \
+    --pre-js "./pre.js" \
+    -o ./build/module.js
+`
+
+// If user doesn't have Emscripten installed locally, try launching it through docker.
+if(!$.which('emcc')) {
+  cmd = `docker run --rm -v $(pwd):/src trzeci/emscripten ${cmd}`
+
+  if (!$.which('docker')) {
+    $.echo('This template requires docker to work. Please install docker and try again.',)
+    $.exit(1)
+  }
 }
 
 $.mkdir('-p', 'build')
-$.exec(
-  'docker run --rm -v $(pwd):/src trzeci/emscripten emcc -O2 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=\'["cwrap", "setValue"]\' -s ALLOW_MEMORY_GROWTH=1 -s DYNAMIC_EXECUTION=0 -s TEXTDECODER=0 -s MODULARIZE=1 -s ENVIRONMENT=\'web\' -s EXPORT_NAME="emscripten" --pre-js \'./pre.js\' -o ./build/module.js ./src/main.c',
-)
+$.exec(cmd)
