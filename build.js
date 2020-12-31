7d7798bc631112500a6cb878cefb1dd803d10d90
kjartanm <kmuller@nepgroup.com>
2020-12-31 08:18:32 +0100

    Change emscripten Docker image
    The trzeci/emscripten Docker image is deprecated in favor of an official emscripten docker image.

--- build.js
+++ build.js
@@ -9,5 +9,5 @@ if (!$.which('docker')) {
 
 $.mkdir('-p', 'build')
 $.exec(
-  'docker run --rm -v $(pwd):/src trzeci/emscripten:sdk-tag-1.38.32-64bit emcc -O2 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=\'["cwrap", "setValue"]\' -s ALLOW_MEMORY_GROWTH=1 -s DYNAMIC_EXECUTION=0 -s TEXTDECODER=0 -s MODULARIZE=1 -s ENVIRONMENT=\'web\' -s EXPORT_NAME="emscripten" --pre-js \'./pre.js\' -o ./build/module.js ./src/main.c',
+  'docker run --rm -v $(pwd):/src emscripten/emsdk emcc -O2 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=\'["cwrap", "setValue"]\' -s ALLOW_MEMORY_GROWTH=1 -s DYNAMIC_EXECUTION=0 -s TEXTDECODER=0 -s MODULARIZE=1 -s ENVIRONMENT=\'web\' -s EXPORT_NAME="emscripten" --pre-js \'./pre.js\' -o ./build/module.js ./src/main.c',
 )
